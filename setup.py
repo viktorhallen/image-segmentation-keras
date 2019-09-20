010eccaacdc3bb54a2b72a85f610ea081ef95c5e
Viktor Hallén <viktor.hallen@gmail.com>
2019-09-20 15:31:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 
 setup(name="keras_segmentation",
-      version="0.2.0",
+      version="0.2.1",
       description="Image Segmentation toolkit for keras",
       author="Divam Gupta",
       author_email='divamgupta@gmail.com',
